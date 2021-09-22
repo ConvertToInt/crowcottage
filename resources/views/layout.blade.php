bded133bf110837f4c8c8b28c1e1b3c3f470f4f2
ConvertToInt <tdalgleish7@gmail.com>
2021-09-22 15:11:59 +0100

    installed bulma and began navbar

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Crow Cottage Arts | Scottish Arts</title>
+
+  <link rel="stylesheet" href="css/mystyles.css"> 
+  <style>
+    html{
+      background-color:hsl(0, 0%, 92%);
+    }
+
+    .copper{
+      font-size:1.5rem;
+      font-weight:500; 
+      color:#A6682E;
+      text-shadow: 0 0 1px #7e7e7e;
+    }
+  </style>
+
+  @yield('head')
+</head>
+
+<body>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        {{-- <img src="logo.png" width="412" height="328"> --}}
+      </a>
+  
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+  
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <span class="navbar-item copper">&middot;</span>
+        <a class="navbar-item copper">
+          HOME
+        </a>
+        <span class="navbar-item copper">&middot;</span>
+        <a class="navbar-item copper">
+          ABOUT
+        </a>
+        <span class="navbar-item copper">&middot;</span>
+        <a class="navbar-item copper">
+          SHOP
+        </a>
+        <span class="navbar-item copper">&middot;</span>
+        <a class="navbar-item copper">
+          <img src="logo.png" width="412" height="328">
+        </a>
+        <span class="navbar-item copper">&middot;</span>
+        <a class="navbar-item copper">
+          STAINED GLASS
+        </a>
+        <span class="navbar-item copper">&middot;</span>
+        <a class="navbar-item copper">
+          EVENTS
+        </a>
+        <span class="navbar-item copper">&middot;</span>
+        <a class="navbar-item copper">
+          CONTACT
+        </a>
+        <span class="navbar-item copper">&middot;</span>
+  
+        {{-- <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            More 
+          </a>
+  
+          <div class="navbar-dropdown">
+            <a class="navbar-item">
+              About
+            </a>
+            <a class="navbar-item">
+              Jobs
+            </a>
+            <a class="navbar-item">
+              Contact
+            </a>
+            <hr class="navbar-divider">
+            <a class="navbar-item">
+              Report an issue
+            </a>
+          </div>
+        </div> --}}
+      </div>
+    </div>
+  </nav>
+
+  @yield('content')
+
+</body>
+</html>
