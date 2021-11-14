3e67c7a9e0ff7fe4d64c4748c07fbb9ff43cfd41
ConvertToInt <tdalgleish7@gmail.com>
2021-11-14 12:03:22 +0000

    success page visuals

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -6,6 +6,28 @@
 
 @section('content')
 
-<h1 class="title has-text-weight-bold has-text-grey-darker is-size-5 mb-5 ml-3">Payment Successful</h1>
+<div class="columns is-centered">
+    <div class="column is-8">
+        <h1 class="title is-size-4 underlined mt-6">Payment Successful - Thank you!</h1>
+
+        <p class="is-size-5 mb-6 mt-3">A Confirmation Email has Been Sent to Your Email Address.</p>
+
+        @foreach($products as $product)
+            <div class="columns mt-2 mb-2 is-centered" id="product_{{$product->id}}">
+                <div class="column is-3">
+                    <a href="{{route('product_show', $product->title)}}">
+                        <figure class="image is-4by3">
+                            <img src="{{asset('storage/' . $product->img)}}" alt="Product image" class="img_shadow">
+                        </figure>
+                    </a>
+                </div>
+                <div class="column">
+                    <h2 class="underlined is-size-5">{{$product->title}}</h2><br>
+                    <h3 class="is-inline">£{{$product->price}} x 1</h3>
+                </div>
+            </div>   
+        @endforeach
+    </div>
+</div>
 
 @endsection
